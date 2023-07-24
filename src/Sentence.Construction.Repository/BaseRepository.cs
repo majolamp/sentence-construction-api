9818d7018c28cfea901921b8d6480becba25fe54
majolamp <mmajola@mrpricegroup.com>
2023-07-24 08:23:31 +0200

    added baseRepository and connection string to the repository library

---
+++ src/Sentence.Construction.Repository/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sentence.Construction.Repository
+{
+    public class BaseRepository
+    {
+        public IDbConnection DapperConnection
+        {
+            get { return new SqlConnection(ConfigurationManager.ConnectionStrings["Default"].ToString()); }
+        }
+    }
+}
---
+++ src/Sentence.Construction.Repository/ConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence.Construction.Repository
+{
+    public sealed class ConnectionString
+    {
+        public string Value { get;}
+        public ConnectionString(string value) => Value = value;
+
+    }
+}
