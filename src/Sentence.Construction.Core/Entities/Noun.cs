f29e614c57725bd5be6d721a11913fc813e68dec
majolamp <mmajola@mrpricegroup.com>
2023-07-24 08:33:25 +0200

    added entity noun

---
+++ src/Sentence.Construction.Core/Entities/Noun.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence.Construction.Core.Entities
+{
+    public class Noun
+    {
+        public int nounId { get; set; }
+        public string nounName { get; set; }
+    }
+}
